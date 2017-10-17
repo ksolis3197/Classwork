44f55b5d88cc476be8be64dc1f4e17c6f02bf739
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-10-17 13:01:24 -0400

    Adding Files to Git

---
+++ ShapeLab/src/shapes/ShapeUtilities.java
@@ -0,0 +1,12 @@
+package shapes;
+
+import java.util.Random;
+
+public class ShapeUtilities {
+	public static Shape getRandomShape()
+	{
+		Random r = newRandom();
+		int x = r.nextInt(3);
+		
+	}
+}
