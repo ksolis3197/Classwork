b8851ac17b8b8b9b908d2d7720e06cd5eaf1fbbc
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-11-15 12:15:03 -0500

    binarysearch 2.00

---
+++ JavaHard/src/BinarySearch2.java
@@ -0,0 +1,42 @@
+
+public class BinarySearch2 {
+	
+	public static void main (String[] args)
+	{
+		int[] test1 = {0,1,2,3,4,5,6,7,8,9};
+		int[] test2 = {0,1,2,3,4,5,6,7,8};
+		
+		for (int i = -1; i< test1.length+1; i++)
+		{
+			System.out.print(binarySearch(test1, i , 0, test1.length));			
+		}
+		System.out.println();
+		for ( int i = -1; i< test2.length+1;i++)
+		{
+			System.out.print(binarySearch(test2, i, 0, test2.length));
+		}
+		System.out.println();
+	}
+	
+	public static int binarySearch (int[] nums, int query, int first, int last)
+	{
+		if( first < last)
+				{
+					int index = (first + last)/2;
+					int guess = nums[index];
+					
+					if( guess == query)
+					{
+						return index;
+					}
+					if (guess > query)
+					{
+						return binarySearch(nums, query, first, index) ;
+					}
+					
+					return binarySearch(nums, query, index+1, last);
+				}
+		
+		return -1;
+	}
+}
